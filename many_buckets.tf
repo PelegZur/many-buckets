54dcf1eabe35453585a7bc0422bd134e775ca3f5
PelegZur <57677867+PelegZur@users.noreply.github.com>
2025-06-04 11:29:02 +0300

    Create many_buckets.tf

---
+++ many_buckets.tf
@@ -0,0 +1,599 @@
+resource "aws_s3_bucket" "many-buckets-0" {
+  bucket          = "many-buckets-0"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-1" {
+  bucket          = "many-buckets-1"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-2" {
+  bucket          = "many-buckets-2"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-3" {
+  bucket          = "many-buckets-3"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-4" {
+  bucket          = "many-buckets-4"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-5" {
+  bucket          = "many-buckets-5"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-6" {
+  bucket          = "many-buckets-6"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-7" {
+  bucket          = "many-buckets-7"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-8" {
+  bucket          = "many-buckets-8"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-9" {
+  bucket          = "many-buckets-9"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-10" {
+  bucket          = "many-buckets-10"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-11" {
+  bucket          = "many-buckets-11"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-12" {
+  bucket          = "many-buckets-12"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-13" {
+  bucket          = "many-buckets-13"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-14" {
+  bucket          = "many-buckets-14"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-15" {
+  bucket          = "many-buckets-15"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-16" {
+  bucket          = "many-buckets-16"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-17" {
+  bucket          = "many-buckets-17"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-18" {
+  bucket          = "many-buckets-18"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-19" {
+  bucket          = "many-buckets-19"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-20" {
+  bucket          = "many-buckets-20"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-21" {
+  bucket          = "many-buckets-21"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-22" {
+  bucket          = "many-buckets-22"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-23" {
+  bucket          = "many-buckets-23"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-24" {
+  bucket          = "many-buckets-24"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-25" {
+  bucket          = "many-buckets-25"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-26" {
+  bucket          = "many-buckets-26"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-27" {
+  bucket          = "many-buckets-27"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-28" {
+  bucket          = "many-buckets-28"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-29" {
+  bucket          = "many-buckets-29"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-30" {
+  bucket          = "many-buckets-30"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-31" {
+  bucket          = "many-buckets-31"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-32" {
+  bucket          = "many-buckets-32"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-33" {
+  bucket          = "many-buckets-33"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-34" {
+  bucket          = "many-buckets-34"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-35" {
+  bucket          = "many-buckets-35"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-36" {
+  bucket          = "many-buckets-36"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-37" {
+  bucket          = "many-buckets-37"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-38" {
+  bucket          = "many-buckets-38"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-39" {
+  bucket          = "many-buckets-39"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-40" {
+  bucket          = "many-buckets-40"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-41" {
+  bucket          = "many-buckets-41"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-42" {
+  bucket          = "many-buckets-42"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-43" {
+  bucket          = "many-buckets-43"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-44" {
+  bucket          = "many-buckets-44"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-45" {
+  bucket          = "many-buckets-45"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-46" {
+  bucket          = "many-buckets-46"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-47" {
+  bucket          = "many-buckets-47"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-48" {
+  bucket          = "many-buckets-48"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-49" {
+  bucket          = "many-buckets-49"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-50" {
+  bucket          = "many-buckets-50"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-51" {
+  bucket          = "many-buckets-51"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-52" {
+  bucket          = "many-buckets-52"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-53" {
+  bucket          = "many-buckets-53"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-54" {
+  bucket          = "many-buckets-54"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-55" {
+  bucket          = "many-buckets-55"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-56" {
+  bucket          = "many-buckets-56"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-57" {
+  bucket          = "many-buckets-57"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-58" {
+  bucket          = "many-buckets-58"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-59" {
+  bucket          = "many-buckets-59"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-60" {
+  bucket          = "many-buckets-60"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-61" {
+  bucket          = "many-buckets-61"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-62" {
+  bucket          = "many-buckets-62"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-63" {
+  bucket          = "many-buckets-63"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-64" {
+  bucket          = "many-buckets-64"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-65" {
+  bucket          = "many-buckets-65"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-66" {
+  bucket          = "many-buckets-66"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-67" {
+  bucket          = "many-buckets-67"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-68" {
+  bucket          = "many-buckets-68"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-69" {
+  bucket          = "many-buckets-69"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-70" {
+  bucket          = "many-buckets-70"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-71" {
+  bucket          = "many-buckets-71"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-72" {
+  bucket          = "many-buckets-72"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-73" {
+  bucket          = "many-buckets-73"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-74" {
+  bucket          = "many-buckets-74"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-75" {
+  bucket          = "many-buckets-75"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-76" {
+  bucket          = "many-buckets-76"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-77" {
+  bucket          = "many-buckets-77"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-78" {
+  bucket          = "many-buckets-78"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-79" {
+  bucket          = "many-buckets-79"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-80" {
+  bucket          = "many-buckets-80"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-81" {
+  bucket          = "many-buckets-81"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-82" {
+  bucket          = "many-buckets-82"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-83" {
+  bucket          = "many-buckets-83"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-84" {
+  bucket          = "many-buckets-84"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-85" {
+  bucket          = "many-buckets-85"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-86" {
+  bucket          = "many-buckets-86"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-87" {
+  bucket          = "many-buckets-87"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-88" {
+  bucket          = "many-buckets-88"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-89" {
+  bucket          = "many-buckets-89"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-90" {
+  bucket          = "many-buckets-90"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-91" {
+  bucket          = "many-buckets-91"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-92" {
+  bucket          = "many-buckets-92"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-93" {
+  bucket          = "many-buckets-93"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-94" {
+  bucket          = "many-buckets-94"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-95" {
+  bucket          = "many-buckets-95"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-96" {
+  bucket          = "many-buckets-96"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-97" {
+  bucket          = "many-buckets-97"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-98" {
+  bucket          = "many-buckets-98"
+  acl             = "public-read"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "many-buckets-99" {
+  bucket          = "many-buckets-99"
+  acl             = "public-read"
+  force_destroy = true
+}
